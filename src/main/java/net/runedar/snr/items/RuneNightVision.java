f401a52e109a1cc28e68a4a8de8a66097ef6add9
KylePC <Kyle1505@outlook.com>
2021-08-10 19:36:59 +0800

    added rune of nightvision

---
+++ src/main/java/net/runedar/snr/items/RuneNightVision.java
@@ -0,0 +1,31 @@
+package net.runedar.snr.items;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class RuneNightVision extends Item {
+    public RuneNightVision(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        ItemStack itemStack = playerEntity.getStackInHand(hand);
+        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 200, 1));
+
+        playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        if (!playerEntity.getAbilities().creativeMode) {
+            itemStack.decrement(1);
+        }
+
+    return TypedActionResult.success(playerEntity.getStackInHand(hand));
+
+    }
+}
