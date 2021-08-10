bb89d8093049b9cac12cffa74f1f40559cca3891
HatLaptop <flint9877@gmail.com>
2021-08-10 14:21:17 +0800

    Mixins added

--- src/main/java/net/runedar/snr/mixin/LivingEntityMixin.java
+++ src/main/java/net/runedar/snr/mixin/LivingEntityMixin.java
@@ -7,9 +7,3 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public class LivingEntityMixin {
-    @Inject(at = @At("HEAD"), method = "tick")
-    private void tick (CallbackInfo info){
-    }
-}
