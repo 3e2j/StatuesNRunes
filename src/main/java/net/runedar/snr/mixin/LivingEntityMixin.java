1051a2a9052d1b22735ea24a27f630db7c3d9b1b
HatLaptop <flint9877@gmail.com>
2021-08-10 13:11:21 +0800

    Another quick push before main GUI sync begins

---
+++ src/main/java/net/runedar/snr/mixin/LivingEntityMixin.java
@@ -0,0 +1,15 @@
+package net.runedar.snr.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    @Inject(at = @At("HEAD"), method = "tick")
+    private void tick (CallbackInfo info){
+    }
+}
