658f4cdf37fa3a7bf0362cee0eee0dbb5d642965
hat <flint9877@gmail.com>
2021-08-21 15:17:26 +0800

    Somewhat decent version of effects in statues. Fades instantly though - needs tick fixing.

--- src/main/java/net/runedar/snr/blocks/blockentities/StatueBlockEntity.java
+++ src/main/java/net/runedar/snr/blocks/blockentities/StatueBlockEntity.java
@@ -2,6 +2,9 @@ package net.runedar.snr.blocks.blockentities;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
@@ -14,17 +17,23 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.runedar.snr.registry.ModBlocks;
 import net.runedar.snr.registry.ModItems;
 import net.runedar.snr.screenhandler.BoxScreenHandler;
 import net.runedar.snr.screenhandler.InventoryCode;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.List;
 
 
 public class StatueBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, InventoryCode, SidedInventory{
-    static PlayerEntity playerEntity;
     public int itemin;
+    @Nullable
+    StatusEffect primary;
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
 
     public StatueBlockEntity(BlockPos pos, BlockState state) {
@@ -53,46 +62,65 @@ public class StatueBlockEntity extends BlockEntity implements NamedScreenHandler
 
     public static void tick(World world, BlockPos pos, BlockState state, StatueBlockEntity blockEntity) {
         ItemStack itemStack = blockEntity.inventory.get(0);
+        @Nullable StatusEffect primaryEffect;
         if (blockEntity.itemin <= 0) {
-            if (itemStack.isOf(ModItems.GOLDEN_HEART)) {
-                blockEntity.itemin = 1;
-            }
-            if (itemStack.isOf(ModItems.CORRUPTED_HEART)) {
-                blockEntity.itemin = 2;
-            }
-            if (itemStack.isOf(ModItems.RUNE_LEVITATION)) {
-                blockEntity.itemin = 3;
-            }
-            if (itemStack.isOf(ModItems.RUNE_SLOWFALL)) {
-                blockEntity.itemin = 4;
-            }
-            if (itemStack.isOf(ModItems.RUNE_NIGHTVISION)) {
-                blockEntity.itemin = 5;
-            }
-            if (itemStack.isOf(ModItems.RUNE_GLOWING)) {
-                blockEntity.itemin = 6;
-            }
-            if (itemStack.isOf(ModItems.RUNE_HEALTHBOOST)) {
-                blockEntity.itemin = 7;
-            }
-            if (itemStack.isOf(ModItems.RUNE_ABSORPTION)) {
-                blockEntity.itemin = 8;
-            }
-            if (itemStack.isOf(ModItems.RUNE_FIRERESISTANCE)) {
-                blockEntity.itemin = 9;
-            }
-            if (itemStack.isOf(ModItems.RUNE_JUMPBOOST)) {
-                blockEntity.itemin = 10;
-            }
-            if (itemStack.isOf(ModItems.RUNE_INVISIBILITY)) {
-                blockEntity.itemin = 11;
-            }
+                if (itemStack.isOf(ModItems.GOLDEN_HEART)) {
+                    blockEntity.itemin = 1;
+                }
+                if (itemStack.isOf(ModItems.CORRUPTED_HEART)) {
+                    blockEntity.itemin = 2;
+                }
+                if (itemStack.isOf(ModItems.RUNE_LEVITATION)) {
+                    blockEntity.itemin = 3;
+                }
+                if (itemStack.isOf(ModItems.RUNE_SLOWFALL)) {
+                    blockEntity.itemin = 4;
+                }
+                if (itemStack.isOf(ModItems.RUNE_NIGHTVISION)) {
+                    blockEntity.itemin = 5;
+                }
+                if (itemStack.isOf(ModItems.RUNE_GLOWING)) {
+                    blockEntity.itemin = 6;
+                }
+                if (itemStack.isOf(ModItems.RUNE_HEALTHBOOST)) {
+                    blockEntity.itemin = 7;
+                }
+                if (itemStack.isOf(ModItems.RUNE_ABSORPTION)) {
+                    blockEntity.itemin = 8;
+                }
+                if (itemStack.isOf(ModItems.RUNE_FIRERESISTANCE)) {
+                    blockEntity.itemin = 9;
+                }
+                if (itemStack.isOf(ModItems.RUNE_JUMPBOOST)) {
+                    blockEntity.itemin = 10;
+                }
+                if (itemStack.isOf(ModItems.RUNE_INVISIBILITY)) {
+                    blockEntity.itemin = 11;
+                }
+            applyPlayerEffects(world, pos, blockEntity.primary, blockEntity.itemin);
         }
         else if (itemStack.isEmpty()) {
             blockEntity.itemin = 0;
         }
         markDirty(world, pos, state);
     }
+
+    private static void applyPlayerEffects(World world, BlockPos pos, @Nullable StatusEffect primaryEffect, int itemin) {
+        double d = 80;
+        Box box = (new Box(pos)).expand(d).stretch(0.0D, world.getHeight(), 0.0D);
+        List<PlayerEntity> list = world.getNonSpectatingEntities(PlayerEntity.class, box);
+        Iterator<PlayerEntity> var1 = list.iterator();
+
+        PlayerEntity playerEntity;
+        while (var1.hasNext()) {
+            playerEntity = var1.next();
+            switch (itemin){
+                case 1: playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 40, 0, true, true));
+                case 2: playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 40, 0, true, true));
+            }
+        }
+    }
+
  
     @Override
     public void readNbt(NbtCompound nbt) {
